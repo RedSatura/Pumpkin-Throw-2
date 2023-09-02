3c4b0fcb0eef6ce32acb55615a8785aae60e082f
RedSatura <111181391+RedSatura@users.noreply.github.com>
2023-09-02 20:31:06 +0800

    projectile launching complete

--- Level Objects/Cannon/cannon.gd
+++ Level Objects/Cannon/cannon.gd
@@ -3,14 +3,18 @@ extends Polygon2D
 onready var pumpkin_spawner = $PumpkinSpawner
 
 var cannon_disabled = false
+var force = 1000
+var angle = 0
 
 func _unhandled_input(_event):
 	if !cannon_disabled:
 		if Input.is_action_just_pressed("cannon_fire"):
 			var pumpkin = load("res://Level Objects/Pumpkin/pumpkin.tscn").instance()
-			
-			get_tree().get_root().add_child(pumpkin)
+			pumpkin.initial_force = force
 			pumpkin.global_position = pumpkin_spawner.global_position
+			pumpkin.cannon_angle = angle * -1
+			print(angle)
+			get_tree().get_root().add_child(pumpkin)
 			cannon_disabled = true
 
 func _physics_process(_delta):
@@ -23,3 +27,5 @@ func _physics_process(_delta):
 			self.rotation_degrees += 1
 			if self.rotation_degrees > 0:
 				self.rotation_degrees = 0
+				
+		angle = deg2rad(self.rotation_degrees)
--- Level Objects/Pumpkin/pumpkin.gd
+++ Level Objects/Pumpkin/pumpkin.gd
@@ -1,14 +1,28 @@
 extends KinematicBody2D
 
-const gravity = 9.8
+const gravity = 150
+
 var velocity = Vector2(0, 0)
 var weight = 0
+var initial_force = 0
+var cannon_angle = 0
 
 var rand_rotation_value = 0
 
 func _ready():
 	randomize()
-	rand_rotation_value
+	rand_rotation_value = rand_range(-10, 10)
+	$Camera2D.current = true
+	velocity.y = -initial_force * sin(cannon_angle)
+	velocity.x = initial_force * cos(cannon_angle)
+	
+	#angle = deg2rad(angle)
+	
+	#velocity.x *= rad2deg(cos(angle))
+	#velocity.y *= rad2deg(sin(angle))
 
 func _physics_process(delta):
-	pass
+	self.rotation_degrees += rand_rotation_value
+	print(velocity)
+	velocity.y += gravity * delta
+	var collision = move_and_collide(velocity * delta)
